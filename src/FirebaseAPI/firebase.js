731046b53fba441a83758d4393217c0aaa1fc7c9
Prystal <ali.axecorp@gmail.com>
2023-03-03 06:31:46 +0500

    update fields

--- src/FirebaseAPI/firebase.js
+++ src/FirebaseAPI/firebase.js
@@ -3,12 +3,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyB0EOxJduz6MhCX4KMXyNzQmzQx0bySgc0",
-	authDomain: "betty-510e8.firebaseapp.com",
-	projectId: "betty-510e8",
-	storageBucket: "betty-510e8.appspot.com",
-	messagingSenderId: "188341672056",
-	appId: "1:188341672056:web:321349f79c7c8a6c03758d",
+  apiKey: "AIzaSyB0EOxJduz6MhCX4KMXyNzQmzQx0bySgc0",
+  authDomain: "betty-510e8.firebaseapp.com",
+  databaseURL: "https://betty-510e8-default-rtdb.firebaseio.com",
+  projectId: "betty-510e8",
+  storageBucket: "betty-510e8.appspot.com",
+  messagingSenderId: "188341672056",
+  appId: "1:188341672056:web:321349f79c7c8a6c03758d"
 };
 
 const app = initializeApp(firebaseConfig);
